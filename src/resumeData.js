bba39b981094006224fd9fa7a95387642f07fdec
Stanley <aynbunda@gmail.com>
2021-09-21 13:44:47 -0400

    resumedata.js

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,106 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
+  "imagebaseurl":"https://alanaynbund.github.io/",
+  "name": "Alan Aynbund",
+  "role": "Computer Science",
+  "linkedinId":"LinkedIn ID",
+  "skypeid": "Skype ID",
+  "roleDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet diam lacinia augue viverra laoreet sit amet et enim. Morbi molestie, dui nec lobortis imperdiet, lacus quam bibendum urna,",
+  "socialLinks":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/alanaynbund-67ba08121/",
+        "className":"fa fa-linkedin"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "name":"github",
+        "url":"http://github.com/alanaynbund",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://twitter.com/aaynbund",
+        "className":"fa fa-twitter"
       }
     ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "aboutme":"I like to play games in my freetime",
+  "address":"NJ",
+  "website":"alanaynbund.github.io",
+  "education":[
+    {
+      "UniversityName":"Rutgers code bootcamp",
+      "specialization":"Specialization",
+      "MonthOfPassing":"Aug",
+      "YearOfPassing":"2020",
+      "Achievements":"Achievements"
+    },
+    {
+      "UniversityName":"University",
+      "specialization":"Specialization",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2018",
+      "Achievements":"Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Company",
+      "specialization":"specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Achievements"
+    },
+    {
+      "CompanyName":"Company",
+      "specialization":"Specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Achievements"
+    }
+  ],
+  "skillsDescription":"My skills",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",  
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"Sample",
+      "name":"Awesome guy"
+    },
+    {
+      "description":"Sample",
+      "name":"Awesome guy"
+    }
+  ]
+}
+
+export default resumeData
